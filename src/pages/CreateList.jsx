a0e8690df420b9f78e410af38041423c3e3a4334
akazad12 <akazad12@aim.com>
2026-02-23 08:23:37 -0500

    added deleted button to only work on completed todos

--- src/pages/CreateList.jsx
+++ src/pages/CreateList.jsx
@@ -1,81 +1,81 @@
 import { useState } from "react";
 
-
-
-const ToDoElement= ({todo,index,toggleComplete})=>{
+const ToDoElement = ({ todo, index, toggleComplete, deleteTodo }) => {
   return (
-  <li style={{ marginBottom: "10px" }}>
-    <input
-      type="checkbox"
-      checked={todo.isCompleted}
-      onChange={() => toggleComplete(index)}
-    />
+    <li style={{ marginBottom: "10px" }}>
+      <input
+        type="checkbox"
+        checked={todo.isCompleted}
+        onChange={() => toggleComplete(index)}
+      />
 
-    <span>
-      {todo.todo}
-    </span>
-  </li>
-);
-}
+      <span>{todo.todo}</span>
+      <button onClick={() => deleteTodo(index)} disabled={!todo.isCompleted}>
+        Delete
+      </button>
+    </li>
+  );
+};
 export default function CreateList() {
-  const [todos, setTodos] = useState([]);   
-  const [input, setInput] = useState("");  
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
   //Add  TODO:
-  const addTodo = ()=>{
-    if(input==="") return;
-    const newTodo= {
-        todo: input,
-        isCompleted:false
-      }
-    
-    setTodos([...todos,newTodo]);
+  const addTodo = () => {
+    if (input === "") return;
+    const newTodo = {
+      todo: input,
+      isCompleted: false,
+    };
+
+    setTodos([...todos, newTodo]);
     setInput("");
- 
-  }
+  };
 
   //completed Todo
-const onCompletion = (index) => {
-  const updatedTodos = todos.map((todo, i) =>
-    i === index
-      ? { ...todo, isCompleted: true }
-      : todo
-  );
+  const onCompletion = (index) => {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, isCompleted: true } : todo,
+    );
 
-  setTodos(updatedTodos);
-};
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const filtered = todos.filter((_, i) => i !== index);
+    setTodos(filtered);
+  };
 
   return (
     <>
       <h1>Todo List</h1>
 
       {/* Adds a new task */}
-      <input 
-      type="text" 
-      value = {input}
-      placeholder="Add new todo"
-      onChange={(e)=> setInput(e.target.value)} />
+      <input
+        type="text"
+        value={input}
+        placeholder="Add new todo"
+        onChange={(e) => setInput(e.target.value)}
+      />
 
       {/* Button to add new task */}
       <button onClick={addTodo}>Add</button>
-      
+
       {/* ============================================================ */}
 
       {/* List of Todos with delete and edit functionality */}
       <ul>
-      {
-       todos.map((value,index) => {
-        return(
-          <ToDoElement
-            key ={index}
-            todo = {value}
-            index = {index}         
-            toggleComplete={onCompletion} 
-          />
-        )
-      })
-       }
-      
+        {todos.map((value, index) => {
+          return (
+            <ToDoElement
+              key={index}
+              todo={value}
+              index={index}
+              toggleComplete={onCompletion}
+              deleteTodo={deleteTodo}
+            />
+          );
+        })}
       </ul>
     </>
   );
